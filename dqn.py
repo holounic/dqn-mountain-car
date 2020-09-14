1be0edc9384596a723e6f807cb6f734590dc059b
Masha Samsikova <41171543+holounic@users.noreply.github.com>
2020-09-15 00:25:25 +0300

    updated

--- dqn.py
+++ dqn.py
@@ -1,6 +1,4 @@
 import gym
-import time
-%matplotlib inline
 
 import copy
 import random
@@ -12,10 +10,10 @@ import torch
 from torch import nn
 from torch import optim
 import torch.nn.functional as F
+%matplotlib inline
 
 device = torch.device("cuda")
 np.random.seed(22)
-
 
 def calc_reward(prev_state, state, gamma=0.98):
   prev_position, prev_velocity = prev_state
@@ -28,10 +26,12 @@ def calc_reward(prev_state, state, gamma=0.98):
   return mul * math.log(abs(reward) + 1)
 
 def calc_rewards(prev_states, states):
-  return [calc_reward(prev_states[i], states[i]) for i in range(len(prev_states))]
+  return [calc_reward(prev_states[i], states[i]) \
+                      for i in range(len(prev_states))]
 
 class DQN:
-  def __init__(self, gamma=0.98, learning_rate=0.00001, update_targer_every=200, device=torch.device("cpu")):
+  def __init__(self, gamma=0.98, learning_rate=0.00001, 
+               update_targer_every=200, device=torch.device("cpu")):
     self.device = device
 
     self.model = nn.Sequential(
@@ -52,7 +52,7 @@ class DQN:
     self.target_model.to(self.device)
     self.model.to(self.device)
 
-    self.optimizer = optim.RMSprop(self.model.parameters(), lr=learning_rate)
+    self.optimizer = optim.Adam(self.model.parameters(), lr=learning_rate)
     self.update_target_every = update_targer_every
     self.updates = 0
     self.gamma = gamma
@@ -63,7 +63,7 @@ class DQN:
     return self.target_model(state).max(0)[1].item()
 
   def update(self, batch):
-    prev_states, actions, states, _, dones = batch
+    prev_states, actions, states, dones = batch
     rewards = calc_rewards(prev_states, states)
     prev_states = torch.tensor(prev_states).to(self.device).float()
     actions = torch.tensor(actions).to(self.device)
@@ -75,7 +75,8 @@ class DQN:
       target_Q_s_a[dones] = 0
 
     target_Q_s_a = target_Q_s_a * self.gamma + rewards
-    pred_Q_s_a = self.model(prev_states).gather(1, actions.unsqueeze(1)).squeeze(1)
+    pred_Q_s_a = self.model(prev_states) \
+                     .gather(1, actions.unsqueeze(1)).squeeze(1)
     loss = F.smooth_l1_loss(pred_Q_s_a, target_Q_s_a)
 
     self.optimizer.zero_grad()
@@ -101,7 +102,8 @@ class DQNUpdater:
         self.dqn.update(batch)
 
 class EpsilonGreedyAgent:
-  def __init__(self, inner_agent, epsilon_max = 0.5, epsilon_min = 0.1, decay_steps=20000):
+  def __init__(self, inner_agent, epsilon_max = 0.5, 
+               epsilon_min = 0.2, decay_steps=20000):
     self.inner_agent = inner_agent
     self.epsilon_max = epsilon_max
     self.epsilon_min = epsilon_min
@@ -110,44 +112,45 @@ class EpsilonGreedyAgent:
 
   def act(self, state):
     self.steps = min(self.decay_steps, self.steps + 1)
-    epsilon = self.epsilon_max + (self.epsilon_min - self.epsilon_max) * self.steps / self.decay_steps
+    epsilon = self.epsilon_max +\
+              (self.epsilon_min - self.epsilon_max) * \
+              self.steps / self.decay_steps
     if epsilon > np.random.random():
       return np.random.randint(0, 3)
     else:
       return self.inner_agent.act(state)
 
-def play_episode(env, agent:Agent):
+def play_episode(env, agent):
   state = env.reset()
   done = False
   trajectory = []
-  total_reward = 0
+  steps = 0
   while not done:
     action = agent.act(state)
-    new_state, reward, done, _ = env.step(action)
-    pos, _ = new_state
-    total_reward += reward
-    trajectory.append((state, action, new_state, reward, done))
+    new_state, _, done, _ = env.step(action)
+    steps += 1
+    trajectory.append((state, action, new_state, done))
     state = new_state
-  return trajectory, total_reward
+  return trajectory, steps
 
-def train_agent(env, exploration_agent, exploitation_agent, updater, env_steps=20000, exploit_every=20000):
+def train_agent(env, exploration_agent, exploitation_agent, \
+                updater, env_steps=20000, exploit_every=20000):
   steps_count = 0
   exploits = 0
-  rewards = []
   steps = []
+  solved_in = []
   while steps_count < env_steps:
     if exploits * exploit_every <= steps_count:
       exploits += 1
-      _, reward = play_episode(env, exploitation_agent)
-      rewards.append(reward)
+      _, steps_per_episode = play_episode(env, exploitation_agent)
+      solved_in.append(steps_per_episode)
       steps.append(steps)
     else:
-      trajectory, reward = play_episode(env, exploration_agent)
+      trajectory, steps_per_episode = play_episode(env, exploration_agent)
       steps_count += len(trajectory)
       updater.update(trajectory)
-      
-  _, reward = play_episode(env, exploitation_agent)
-  rewards.append(reward)
+  _, steps_per_episode = play_episode(env, exploitation_agent)
+  solved_in.append(steps_per_episode)
   steps.append(steps_count)
   return rewards, steps
 
@@ -157,4 +160,6 @@ steps_per_episode = 200
 dqn_agent = DQN(device=device)
 updater = DQNUpdater(dqn_agent, buffer_size=max_steps)
 epsilon_greedy = EpsilonGreedyAgent(dqn_agent, decay_steps=max_steps)
-rewards, steps = train_agent(env, epsilon_greedy, dqn_agent, updater, env_steps=max_steps, exploit_every=max_steps//steps_per_episode)
+rewards, steps = train_agent(env, epsilon_greedy, dqn_agent, updater, \
+                             env_steps=max_steps, \
+                             exploit_every=max_steps//steps_per_episode)
